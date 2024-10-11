b288c448c717fac205938c9394eeac0e3e2a393d
Giovani <rochagiovani12@gmail.com>
2024-10-10 21:41:07 -0300

    JS 02 - CONSOLE

---
+++ JS/JS 02 - CONSOLE/main.js
@@ -0,0 +1,13 @@
+//variaveis = criar um local namemoria do computador e guardar um dado//
+
+//numero/caracteres especiais/palavras reservadas
+
+//
+
+//quando eu quero criar uma variavel presciso inserir antes do nome dessa variavel as palvras reservads var/let/const//
+
+var nomeAluno = 0
+
+nomeAluno = "Jaozinho"
+
+var 
