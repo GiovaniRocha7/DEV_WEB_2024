dd52428f29f60417bb71a51528952e8784b3e097
Giovani <rochagiovani12@gmail.com>
2024-10-16 21:26:57 -0300

    js 02 console

---
+++ JS/JS 02 - CONSOLE/operadores.js
@@ -0,0 +1,61 @@
+// let nota1, nota2, media;
+// nota1 = 5;
+// nota2 = 10;
+// media = (nota1 + nota2)/ 2
+// console.log(media)
+
+                                                            // OPERADORES RELACIONAIS ABAIXO
+// 'use scrict'
+// let valor, num1, num2;
+// num1 = 10;
+// num2 = 20;
+// valor = num1 > num2;
+// valor = num1 >= num2;
+// valor = num1 < num2;
+// valor = num1 <= num2;
+// valor = num1 == num2;
+// valor = num1 === num2;
+// TYPEOF IDENTIFICA O TIPO DE OPERADOR
+// num1 = 20;
+// num2 = "20"
+// valor = num1 != num2;
+
+// console.log(valor);
+
+                                                           // OPERADORES UNARIOS ABAIXO
+// let nome = "Giovani";
+// let num = 5;
+// let num1 = 10;
+// console.log(typeof num);
+// console.log(num + num1);
+// console.log(nome.length);
+
+// let sol = true;
+// let dinheiro = 300;
+// // 300 >=
+// // sol true
+//     let praia = sol == true && dinheiro >= 300?
+//     "Vamos a praia" : "Não vamos a praia."
+//     console,console.log(praia);
+
+    // && SIGNIFICADO
+    // V + V = V
+    // V + F = F
+    // F + V = F
+    // F + F = F
+    
+    // F + F = F
+    // F + F = F
+    // F + F = F
+    // F + F = F
+
+// && = ou
+    
+let media = 7;
+let falta = 2; 
+
+//  FALTAS > 5 
+
+let res = media >= 7 && falta <= 1 ? "Aprovado" : "Reprovado"; 
+    console.log(res);
+    console.log(res);
