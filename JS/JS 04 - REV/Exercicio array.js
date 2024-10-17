9974f800bab178b4ca303d922ee54c7e3b20f471
Giovani <rochagiovani12@gmail.com>
2024-10-17 20:53:42 -0300

    exercicio array

---
+++ JS/JS 04 - REV/Exercicio array.js
@@ -0,0 +1,8 @@
+let nome = prompt ('Qaul seu nome? :');
+alert ('Ola,${nome}""')
+let idade = prompt ('Qaul sua idade? : ');
+let casa = prompt ('Onde voce mora?: ');
+
+console.log(nome);
+console.log(idade);
+console.log(casa);
