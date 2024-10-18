f4435557233ed543fa1bd6a3a4415be98e86b0b0
Giovani <rochagiovani12@gmail.com>
2024-10-17 21:15:30 -0300

    ex array

--- JS/JS 04 - REV/Exercicio array.js
+++ JS/JS 04 - REV/Exercicio array.js
@@ -1,8 +1,8 @@
-let nome = prompt ('Qaul seu nome? :');
-alert ('Ola,${nome}""')
-let idade = prompt ('Qaul sua idade? : ');
-let casa = prompt ('Onde voce mora?: ');
+// let nome = prompt ('Qaul seu nome? :');
+// alert ('Ola,${nome}""')
+// let idade = prompt ('Qaul sua idade? : ');
+// let casa = prompt ('Onde voce mora?: ');
 
-console.log(nome);
-console.log(idade);
-console.log(casa);
+// console.log(nome);
+// console.log(idade);
+// console.log(casa);
---
+++ JS/JS 04 - REV/substring.js
@@ -0,0 +1,3 @@
+let navegador = 'brave';
+let novaSring = navegador.charAt(0);
+let jonatas = navegador.charAt(3);
