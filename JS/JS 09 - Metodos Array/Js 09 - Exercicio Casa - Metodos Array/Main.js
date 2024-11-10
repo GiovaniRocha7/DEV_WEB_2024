0266e89bc477689cf11bafb39adcdc4d1f071cfb
GiovaniRocha7 <rochagiovani12@gmail.com>
2024-11-10 03:54:29 -0300

    Exercicio Casa 09 - Metodos Array

---
+++ JS/JS 09 - Metodos Array/Js 09 - Exercicio Casa - Metodos Array/Main.js
@@ -0,0 +1,8 @@
+let numeros = [7, 18, 25, 4, 95, 28, 56, 31];
+function ImparPar(numero) {
+    if (numero % 2 === 0){
+        console.log(`${numero} é Impar`);
+    } else{
+        console.log(`${numero} é Par`);        
+    }
+}
