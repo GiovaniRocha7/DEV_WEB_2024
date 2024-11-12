65997f837f0b95564842af2cb6d82a72309da100
GiovaniRocha7 <rochagiovani12@gmail.com>
2024-11-11 23:34:57 -0300

    exercicio js 09

--- JS/JS 09 - Metodos Array/Js 09 - Exercicio 01 Casa - Metodos Array/Main.js
+++ JS/JS 09 - Metodos Array/Js 09 - Exercicio 01 Casa - Metodos Array/Main.js
@@ -1,7 +1,6 @@
-function ImparPar(numero) {
-    if (numero % 2 === 0) {
-        console.log(`${numero} é Par`);
-    } else {
-        console.log(`${numero} é Ímpar`);
-    }
-}
+numeros.forEach(ImparPar);
+
+let = numeros [17, 43, 8, 4, 97, 56, 29, 3, 75, 34, 88, 82, 61, 59];
+let Nfiltros = numeros.filter(numero => numero > 20 && numero < 80);
+console.log(Nfiltros);
+console.log(numeros);
