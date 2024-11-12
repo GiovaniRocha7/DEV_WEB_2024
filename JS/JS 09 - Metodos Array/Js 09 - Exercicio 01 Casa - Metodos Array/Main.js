a1246302c32e079ec96a249a80f3ace2a51b3df8
GiovaniRocha7 <rochagiovani12@gmail.com>
2024-11-11 22:55:58 -0300

    Exercicio Js 09

--- JS/JS 09 - Metodos Array/Js 09 - Exercicio 01 Casa - Metodos Array/Main.js
+++ JS/JS 09 - Metodos Array/Js 09 - Exercicio 01 Casa - Metodos Array/Main.js
@@ -1,9 +1,7 @@
-let numeros = [17, 43, 8, 4, 97, 56, 29, 95];
 function ImparPar(numero) {
-    if (numero % 2 === 0){
-        console.log(`${numero} é Impar`);
-    } else{
-        console.log(`${numero} é Par`);        
+    if (numero % 2 === 0) {
+        console.log(`${numero} é Par`);
+    } else {
+        console.log(`${numero} é Ímpar`);
     }
 }
-numeros.forEach(ImparPar);
