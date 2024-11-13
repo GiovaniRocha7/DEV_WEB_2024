dcac1bbbd6fdf255a428d3a137d356d6762ec737
Giovani <rochagiovani12@gmail.com>
2024-11-12 21:24:40 -0300

    Js 11 Dom

---
+++ JS 11 - Dom/Main.js
@@ -0,0 +1,14 @@
+document.body.style.backgroundColor = "black"
+
+document.title = "socorro dom"
+
+document.documentElement.lang = "pt-br"
+
+document.createElement('button')
+
+let campoNovo = document.createElement ('button')
+
+campoNovo.style.backgroundColor = "red"
+campoNovo.style.borderRadius = "5px"
+
+Document.body.appendChild(campoNovo).innnerHTML = "Enviar"
