ea458838558228427f7b2ad72a8994cb7fdce1a6
Giovani <rochagiovani12@gmail.com>
2024-11-18 20:52:50 -0300

    ex js casa 10 Classe

---
+++ JS/JS 10 - Classes/Exercicio Casa - Js 10 Classes/Main.js
@@ -0,0 +1,12 @@
+class ContaBancaria (nome, idade, salário, sexo, agência, conta, numeroConta.){
+    constructor 
+    this.nome = nome
+    this.idade = idade
+    this.salário = salario
+    this.sexo = sexo
+    this.agência = agência
+    this.conta = conta
+    this.numeroConta = numeroConta
+}
+
+
